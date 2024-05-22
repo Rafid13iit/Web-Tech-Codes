cfac5adc614b01b9c87d7e8205d2f8d2ff1bf0d9
Rafid13iit <rafidssrr.6767@gmail.com>
2024-05-22 23:29:12 +0600

    semester final preparation codes added

---
+++ Semester Final Practise/popUpBoxes.js
@@ -0,0 +1,18 @@
+// Alert box
+alert("This is an alert box!");
+
+
+// Cofirm box
+var userConfirmed = confirm("Do you want to proceed?");
+if (userConfirmed) {
+    // User clicked OK
+} else {
+    // User clicked Cancel
+}
+
+
+// Prompt box
+var userInput = prompt("Please enter your name:", "Harry Potter");
+if (userInput !== null) {
+    // User entered something
+}
